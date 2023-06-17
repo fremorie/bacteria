31242e978416187d15560314f82f8591bc44af9e
Daria Borisiak <daria.borisiak@gmail.com>
2023-06-17 12:12:40 +0200

    fix: adjust reactor border-radius

--- src/components/Reactor/styles.ts
+++ src/components/Reactor/styles.ts
@@ -7,8 +7,8 @@ export const Container = styled.div`
 const canvasTopDefault = 156;
 const canvasLeftDefault = 229;
 export const imgSizeDefault = 800;
-export const canvasHeightDefault = 559;
-export const canvasWidthDefault = 342;
+export const canvasHeightDefault = 562;
+export const canvasWidthDefault = 344;
 
 const canvasTop = `${canvasTopDefault / (imgSizeDefault / 100)}%`;
 const canvasLeft = `${canvasLeftDefault / (imgSizeDefault / 100)}%`;
@@ -19,7 +19,7 @@ export const ReactorImg = styled.img`
 
 export const Canvas = styled.canvas`
   border: 2px solid black;
-  border-radius: 0 0 200px 200px;
+  border-radius: 0 0 70% 70% / 0 0 42% 42%;
   position: absolute;
   top: ${canvasTop};
   left: ${canvasLeft};
